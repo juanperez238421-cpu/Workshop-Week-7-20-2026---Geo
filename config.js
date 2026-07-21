984ef7fd0b6a4cb9abfe3952dcab66f4a920d115
juanperez238421-cpu <juanperez238421@gmail.com>
2026-07-21 18:39:21 -0500

    Configure current Render server and live game URL

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.TRIAD_CONFIG = Object.freeze({
-  serverUrl: "wss://triad-territory-rush-server.onrender.com"
+  serverUrl: "wss://triad-territory-rush-server.onrender.com",
+  gameUrl: "https://juanperez238421-cpu.github.io/Workshop-Week-7-20-2026---Geo/"
 });

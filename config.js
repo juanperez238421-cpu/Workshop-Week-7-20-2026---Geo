a1cea435d5f9275d460a864b966454010a3a49ea
juanperez238421-cpu <juanperez238421@gmail.com>
2026-07-21 20:36:34 -0500

    Point shared links to dedicated role pages

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.TRIAD_CONFIG = Object.freeze({
   serverUrl: "wss://triad-territory-rush-server.onrender.com",
-  gameUrl: "https://juanperez238421-cpu.github.io/Workshop-Week-7-20-2026---Geo/"
+  gameUrl: "https://juanperez238421-cpu.github.io/Workshop-Week-7-20-2026---Geo/student.html",
+  teacherUrl: "https://juanperez238421-cpu.github.io/Workshop-Week-7-20-2026---Geo/teacher.html"
 });

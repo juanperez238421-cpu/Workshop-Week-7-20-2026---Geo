1d9f610c0857ec2549d042b9cc3a17fde9e49c9c
juanperez238421-cpu <juanperez238421@gmail.com>
2026-07-23 08:27:45 -0500

    Bump student client to master-view aim v21

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TRIAD_CONFIG = Object.freeze({
   serverUrl: "wss://triad-territory-rush-server.onrender.com",
   gameUrl: "https://juanperez238421-cpu.github.io/Workshop-Week-7-20-2026---Geo/",
-  clientBuild: "20260723-fluidgameplay20"
+  clientBuild: "20260723-masterviewaim21"
 });

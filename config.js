63654243313a028c2053965dd0d613b2f06a1cc4
juanperez238421-cpu <juanperez238421@gmail.com>
2026-07-22 22:37:35 -0500

    Bump student client cache key for input hotfix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TRIAD_CONFIG = Object.freeze({
   serverUrl: "wss://triad-territory-rush-server.onrender.com",
   gameUrl: "https://juanperez238421-cpu.github.io/Workshop-Week-7-20-2026---Geo/",
-  clientBuild: "20260723-studentrecovery16"
+  clientBuild: "20260723-inputhotfix17"
 });
